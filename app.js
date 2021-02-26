aa6178f4dc32aa73ecbcfaa109f25c31b0b7ec5c
Isaac Pontes <isaaclfp@outlook.com>
2021-02-25 21:49:47 -0300

    Added CORS.

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ require('./config/database');
 var express = require('express');
 var path = require('path');
 var logger = require('morgan');
+var cors = require('cors');
 
 var usersRouter = require('./app/routes/users');
 var notesRouter = require('./app/routes/notes');
@@ -13,6 +14,7 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(cors());
 
 app.use('/users', usersRouter);
 app.use('/notes', notesRouter);
